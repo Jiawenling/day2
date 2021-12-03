f0e30bda6e60c02d1793bae358b9172b1024eb26
Jia Wen <jling002@e.ntu.edu.sg>
2021-12-03 16:59:01 +0800

    trying out

---
+++ bankaccount/src/main/java/day2/BankAccount.java
@@ -0,0 +1,17 @@
+package day2;
+
+import java.util.List;
+
+public class BankAccount {
+    private String name;
+    private String accNumber;
+    private float balance;
+    private List<String> transactions;
+    private boolean closed;
+    private String creationDate;
+    private String closingDate;
+
+
+
+
+}
